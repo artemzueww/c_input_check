2db1c904d671c3a26672b75e4c94ff190303cd7b
artemzueww <artemzueww@gmail.com>
2025-12-10 22:35:54 +0300

    Update input_check.c

--- input_check.c
+++ input_check.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
 
-int input_check(int* a) {
+int input_check(int *a)
+{
     int res = scanf("%d", a);
-    if (res != 1) {
+    if (res != 1)
+    {
         return 0;
     }
 
     char extra = getchar();
-    if (extra != '\n' && extra != EOF') {
+    if (extra != '\n' && extra != EOF)
+    {
         return 0;
     }
 
     return 1;
 }
 
-int main(void) {
+int main(void)
+{
     int a;
 
-    if (!input_check(&a)) {
+    if (!input_check(&a))
+    {
         printf("n/a\n");
         return 0;
     }
