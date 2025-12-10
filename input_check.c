9c109acc19bfb425c9ef47392f12b250dabecfa6
artemzueww <artemzueww@gmail.com>
2025-12-10 22:32:20 +0300

    Create input_check.c

---
+++ input_check.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int input_check(int* a) {
+    int res = scanf("%d", a);
+    if (res != 1) {
+        return 0;
+    }
+
+    char extra = getchar();
+    if (extra != '\n' && extra != EOF') {
+        return 0;
+    }
+
+    return 1;
+}
+
+int main(void) {
+    int a;
+
+    if (!input_check(&a)) {
+        printf("n/a");
+        return 0;
+    }
+
+    printf("\nOK\n%d\n", a);
+    return 0;
+}
